db06c5b7c3c6e32b4a00012166e4bd25efd416f3
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-05-15 09:42:52 +0800

    Add database class method

---
+++ assets/dist/php/class/database.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class Database {
+    public function createDatabase(string $dbname) {
+        $connect = new PDO("mysql:host=localhost;", "admin", "root");
+        $query = "CREATE DATABASE IF NOT EXISTS $dbname";
+        $query_execute = $connect->prepare($query);
+        $query_execute->execute();
+    }
+
+    public function createTable(string $table) {
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $connect = new PDO("mysql:host=localhost;dbname=echat", "admin", "root", $options);
+
+        if ($table == "accounts") {
+            $sql = "CREATE TABLE IF NOT EXISTS $table (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                type enum('user','admin') DEFAULT 'user',
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+            $connect->exec($sql);
+        } else if ($table == "chats") {
+            $sql = "CREATE TABLE IF NOT EXISTS $table (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                account_ID INT NOT NULL,
+                message VARCHAR(255),
+                CONSTRAINT fk_chats_accounts_id FOREIGN KEY (account_ID) REFERENCES accounts(id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+            $connect->exec($sql);
+        }
+    }
+}
