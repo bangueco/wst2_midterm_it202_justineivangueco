f7078019d464418ee0cdb0cc02f78cfe889ddff7
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-05-15 09:41:28 +0800

    Add user interface for profile page

---
+++ assets/dist/php/pages/profile.php
@@ -0,0 +1,57 @@
+<?php
+    session_start();
+    $connect = new PDO("mysql:host=localhost;dbname=echat", 'admin', 'root');
+    $query = "SELECT id, name, username, password, type FROM accounts WHERE username=:username";
+
+    $profile = $connect->prepare($query);
+    $profile->bindValue('username',$_GET['profile']);
+    $profile->execute();
+
+    $records = $profile->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php
+        if ($records[0]['id'] == $_SESSION['auth'][0]['id']) {
+            echo 'Your';
+        } else {
+            echo $records[0]['username'].'\'s';
+        }
+    ?> Profile</title>
+
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/profile.css">
+</head>
+<body>
+    <div class="d-flex justify-content-center align-items-center profile container">
+        <div class="profile-container p-3">
+            <?php
+                if ($records[0]['id'] == $_SESSION['auth'][0]['id']) {
+                    echo '<h1>Your Profile Information</h1>';
+                } else {
+                    echo '<h1>'.$records[0]['name'].'\'s Profile'.' Information</h1>';
+                }
+            ?>
+            <div class="profile-info text-center">
+                <p>Full Name: <?php 
+                if ($records[0]['id'] == $_SESSION['auth'][0]['id']) {
+                    echo $_SESSION['auth'][0]['name'];
+                } else {
+                    echo $records[0]['name'];
+                }?>
+                </p>
+                <p>Username: <?php if ($records[0]['id'] == $_SESSION['auth'][0]['id']) {
+                    echo $_SESSION['auth'][0]['username'];
+                } else {
+                    echo $records[0]['username'];
+                }?></p>
+                <a class="btn btn-danger" href="chatroom.php">Back</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
